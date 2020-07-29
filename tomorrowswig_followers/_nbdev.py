83d59a1f6c2214e9b47a6ee9be6fbd79650b5bd1
manycoding <valeriymukhtarulin@tuta.io>
2020-07-29 17:55:39 +0000

    Wrap up ads insights

--- tomorrowswig_followers/_nbdev.py
+++ tomorrowswig_followers/_nbdev.py
@@ -2,25 +2,34 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"APP_ID": "01_ads.ipynb",
-         "APP_SECRET": "01_ads.ipynb",
-         "TOKEN": "01_ads.ipynb",
-         "USER_ID": "01_ads.ipynb",
-         "SHEET": "01_ads.ipynb",
-         "SH": "01_ads.ipynb",
-         "get_followers": "00_followers.ipynb",
-         "get_df": "00_followers.ipynb",
-         "get_worksheet": "00_followers.ipynb",
-         "write_df": "00_followers.ipynb",
-         "get_updated_followers": "00_followers.ipynb",
-         "save_followers": "00_followers.ipynb",
-         "save_change": "00_followers.ipynb",
-         "make_change": "00_followers.ipynb",
-         "lambda_handler": "00_followers.ipynb",
-         "AD_ACC_ID": "01_ads.ipynb"}
+index = {"APP_ID": "00_core.ipynb",
+         "APP_SECRET": "00_core.ipynb",
+         "TOKEN": "00_core.ipynb",
+         "USER_ID": "00_core.ipynb",
+         "SHEET": "00_core.ipynb",
+         "SH": "00_core.ipynb",
+         "get_df": "00_core.ipynb",
+         "get_worksheet": "00_core.ipynb",
+         "write_df": "00_core.ipynb",
+         "get_followers": "01_followers.ipynb",
+         "get_updated_followers": "01_followers.ipynb",
+         "save_followers": "01_followers.ipynb",
+         "save_change": "01_followers.ipynb",
+         "make_change": "01_followers.ipynb",
+         "lambda_handler": "01_followers.ipynb",
+         "AD_ACC_ID": "02_ads.ipynb",
+         "account": "02_ads.ipynb",
+         "get_dif": "02_ads.ipynb",
+         "COLUMNS": "02_ads.ipynb",
+         "get_insights": "02_ads.ipynb",
+         "get_action": "02_ads.ipynb",
+         "get_followers_change": "02_ads.ipynb",
+         "get_insights_df": "02_ads.ipynb",
+         "create_insights": "02_ads.ipynb"}
 
-modules = ["followers/api.py",
-           "ads/api.py"]
+modules = ["core.py",
+           "followers.py",
+           "ads.py"]
 
 doc_url = "https://manycoding.github.io/tomorrowswig_followers/"
 
